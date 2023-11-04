535622e321b3db39943844b28b0fe00ce5467825
Himanshupatel25 <hkapuriya309@rku.ac.in>
2023-11-04 12:43:15 +0530

    Finished Project.

---
+++ app/src/main/java/com/example/chatmessenger/notifications/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.chatmessenger.notifications
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAAlfX6N90:APA91bGaih0LkeFeoW9G0WwAFau4JQ4_R9rHQL5NQIcgXDWJImACyvWF203ZIBnTuxPaJM9FrPSV56CTIIa1GHsh8q_oaA4S1FRA4Ay1vv2EOa6SuTmIlLx9cm1SMorgkd9UoxaonWZu"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
